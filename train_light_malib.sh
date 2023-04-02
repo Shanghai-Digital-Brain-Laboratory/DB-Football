6325e1492f505c36c3126c9c450121d7ecf50173
YanSong <yansong0902@gmail.com>
2023-04-03 01:52:31 +0800

    add academy scenario content

--- train_light_malib.sh
+++ train_light_malib.sh
@@ -1,1 +1,7 @@
-python light_malib/main_pbt.py --config light_malib/expr/gr_football/expr_10_vs_10_psro.yaml
+#python light_malib/main_pbt.py --config light_malib/expr/gr_football/expr_10_vs_10_psro.yaml
+
+#python light_malib/main_pbt.py --config light_malib/expr/gr_football/expr_4_vs_4_psro.yaml
+
+python light_malib/main_pbt.py --config light_malib/expr/academy/expr_3_vs_1_with_keeper.yaml
+
+
