cd3eeb4e4ba32c25096d9d5aeeb6c76dd60504a6
DiligentPanda <srevir@foxmail.com>
2023-04-18 23:08:22 +0800

    organize cooperation configs

--- train_light_malib.sh
+++ train_light_malib.sh
@@ -1,12 +1,13 @@
-#python light_malib/main_pbt.py --config light_malib/expr/gr_football/expr_10_vs_10_psro.yaml
+# python light_malib/main_pbt.py --config light_malib/expr/competition/expr_10_vs_10_psro.yaml
 
-#python light_malib/main_pbt.py --config light_malib/expr/gr_football/expr_4_vs_4_psro.yaml
+# python light_malib/main_pbt.py --config light_malib/expr/competition/expr_4_vs_4_psro.yaml
 
-#python light_malib/main_pbt.py --config light_malib/expr/academy/expr_3_vs_1_with_keeper.yaml
+# python light_malib/main_pbt.py --config light_malib/expr/cooperation/full_game/expr_10_vs_10.yaml
 
-#python light_malib/main_pbt.py --config light_malib/expr/academy/expr_run_pass_and_shot_with_keeper.yaml
+# python light_malib/main_pbt.py --config light_malib/expr/cooperation/academy/expr_run_pass_and_shot_with_keeper.yaml
 
-# python light_malib/main_pbt.py --config light_malib/expr/gr_football/expr_dqn_4_vs_4.yaml
+# python light_malib/main_pbt.py --config light_malib/expr/competition/expr_dqn_4_vs_4.yaml
 
-python light_malib/main_pbt.py --config light_malib/expr/academy/expr_counterattack.yaml
+# python light_malib/main_pbt.py --config light_malib/expr/cooperation/academy/expr_counterattack.yaml
 
+python light_malib/main_pbt.py --config light_malib/expr/cooperation/academy/expr_3_vs_1_with_keeper.yaml
