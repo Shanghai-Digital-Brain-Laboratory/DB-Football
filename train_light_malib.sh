59b0f5ad38fb1bc203bcfcdb903e6a8375c53cb8
YanSong <yansong0902@gmail.com>
2023-05-05 19:08:27 +0800

    add pomdp encoder

--- train_light_malib.sh
+++ train_light_malib.sh
@@ -12,5 +12,10 @@
 
 #python light_malib/main_pbt.py --config light_malib/expr/cooperation/academy/expr_3_vs_1_with_keeper.yaml
 
-python light_malib/main_pbt.py --config light_malib/expr/qmix/expr_3_vs_1_with_keeper_qmix.yaml
+#python light_malib/main_pbt.py --config light_malib/expr/cooperation/full_game/expr_full_game_4_vs_4_easy.yaml
 
+#python light_malib/main_pbt.py --config light_malib/expr/mappo/expr_3_vs_1_with_keeper_mappo.yaml
+
+#python light_malib/main_pbt.py --config light_malib/expr/bc/expr_full_game_4_vs_4_bc.yaml
+
+python light_malib/main_pbt.py --config light_malib/expr/mappo/expr_3_vs_1_with_keeper_pomdp_mappo.yaml
