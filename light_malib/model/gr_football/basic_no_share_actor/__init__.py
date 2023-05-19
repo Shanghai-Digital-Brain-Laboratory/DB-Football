5396bfa176fb046beccc005c6b5d8b255a747d35
DiligentPanda <srevir@foxmail.com>
2023-05-19 17:32:04 +0800

    only not share actor; add no share actor for mappo

--- light_malib/model/gr_football/basic_no_share_actor/__init__.py
+++ light_malib/model/gr_football/basic_no_share_actor/__init__.py
@@ -95,52 +95,52 @@ class Actor(nn.Module):
         
         return actions, actor_rnn_states, action_log_probs, dist_entropy
   
-# Critic=critic_module.Critic
+Critic=critic_module.Critic
 
-class Critic(nn.Module):
-    def __init__(
-            self,
-            model_config,
-            observation_space,
-            action_space,
-            custom_config,
-            initialization         
-            ) -> None:
-        super().__init__()
+# class Critic(nn.Module):
+#     def __init__(
+#             self,
+#             model_config,
+#             observation_space,
+#             action_space,
+#             custom_config,
+#             initialization         
+#             ) -> None:
+#         super().__init__()
         
-        self.critics=nn.ModuleList()
+#         self.critics=nn.ModuleList()
         
-        self.num_agents=custom_config["num_agents"]
-        for i in range(self.num_agents):
-            critic=critic_module.Critic(model_config,observation_space,action_space,custom_config,initialization)
-            self.critics.append(critic)
+#         self.num_agents=custom_config["num_agents"]
+#         for i in range(self.num_agents):
+#             critic=critic_module.Critic(model_config,observation_space,action_space,custom_config,initialization)
+#             self.critics.append(critic)
             
-        self.rnn_layer_num=critic.rnn_layer_num
-        self.rnn_state_size=critic.rnn_state_size
+#         self.rnn_layer_num=critic.rnn_layer_num
+#         self.rnn_state_size=critic.rnn_state_size
                         
-    def forward(self,observations, rnn_states, rnn_masks):
-        observations_list=fold(observations,self.num_agents)
-        rnn_states_list=fold(rnn_states,self.num_agents)
-        rnn_masks_list=fold(rnn_masks,self.num_agents)
+#     def forward(self,observations, rnn_states, rnn_masks):
+#         observations_list=fold(observations,self.num_agents)
+#         rnn_states_list=fold(rnn_states,self.num_agents)
+#         rnn_masks_list=fold(rnn_masks,self.num_agents)
         
-        _values=[]
-        _rnn_states=[]
+#         _values=[]
+#         _rnn_states=[]
             
-        for i in range(self.num_agents):
-            observations_i=observations_list[:,i]
-            rnn_states_i = rnn_states_list[:,i] if rnn_states_list is not None else None
-            rnn_masks_i = rnn_masks_list[:,i] if rnn_masks_list is not None else None
-            values_i, rnn_states_i = self.critics[i].forward(
-                observations_i,
-                rnn_states_i,
-                rnn_masks_i)
+#         for i in range(self.num_agents):
+#             observations_i=observations_list[:,i]
+#             rnn_states_i = rnn_states_list[:,i] if rnn_states_list is not None else None
+#             rnn_masks_i = rnn_masks_list[:,i] if rnn_masks_list is not None else None
+#             values_i, rnn_states_i = self.critics[i].forward(
+#                 observations_i,
+#                 rnn_states_i,
+#                 rnn_masks_i)
             
-            _values.append(values_i)
-            _rnn_states.append(rnn_states_i)
+#             _values.append(values_i)
+#             _rnn_states.append(rnn_states_i)
             
-        values=torch.stack(_values,dim=1)
-        values=unfold(values,self.num_agents)
-        rnn_states=torch.stack(_rnn_states,dim=1)
-        rnn_states=unfold(rnn_states,self.num_agents)
+#         values=torch.stack(_values,dim=1)
+#         values=unfold(values,self.num_agents)
+#         rnn_states=torch.stack(_rnn_states,dim=1)
+#         rnn_states=unfold(rnn_states,self.num_agents)
         
-        return values, rnn_states
+#         return values, rnn_states
